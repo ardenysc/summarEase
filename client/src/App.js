04327e378a13d826280c6dbe4f89442483b2c7c3
Arden Choi <ardenyschoi@gmail.com>
2023-03-25 16:55:02 -0700

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Summaries from './components/Summaries';
-import Summary from './components/Summary';
+import CustomizedInputBase from './components/CustomizedInputBase';
+import Input from '@mui/material/Input';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Input /> */}
       {/* <Summary /> */}
+      <CustomizedInputBase />
       <Summaries />
     </div>
   );
