bfc458d19b26465be8d49c801b522f2b58b916f3
Horton Lai <hortonht.lai@gmail.com>
2023-03-25 11:54:26 -0700

    completed prompt generator

--- server/controller/gpt-controller.js
+++ server/controller/gpt-controller.js
@@ -1,24 +1,49 @@
 import openai from "../chatgpt/chatgpt.js";
 
-function generatePrompt(url) {
-    return `Provide a summary of this webpage in 50 words.
+function generateSummaryPrompt(url) {
+    return `Provide a summary of this webpage in 60 words.
   
-  Webpage: https://globalnews.ca/news/9577125/edmonton-police-regimental-funeral-procession-road-closures-transit/`;
+  Webpage: ${url}`;
  };
+
+function generateKeywordPrompt(url) {
+  return `Provide 8 keywords for this webpage in comma separated format.
+
+Webpage: ${url}`;
+};
+
+function toStringArray(completionText, delimiter) {
+  return completionText.slice(2).split(delimiter);
+}
 
 export const handleUrl =  async (req, res) => {
     const url = req.body.url;
   
     try {
-      const completion = await openai.createCompletion({
+      const summaryCompletion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: generatePrompt(url),
-        temperature: 0.6,
+        prompt: generateSummaryPrompt(url),
+        temperature: 0.8,
         max_tokens: 200
       });
       //res.status(200).json({ result: completion.data.choices[0].text });
-      console.log(completion.data.choices[0].text);
-      console.log(completion.data);
+      let summaryCompletionText = summaryCompletion.data.choices[0].text;
+      // console.log(summaryCompletion.data.choices[0].text);
+      console.log(toStringArray(summaryCompletionText, ". "));
+
+
+      const keywordCompletion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: generateKeywordPrompt(url),
+        temperature: 0.6,
+        top_p: 0.5,
+        max_tokens: 200
+      });
+      //res.status(200).json({ result: completion.data.choices[0].text });
+      let keywordCompletionText = keywordCompletion.data.choices[0].text;
+      // console.log(keywordCompletion.data.choices[0].text);
+      console.log(toStringArray(keywordCompletionText, ", "));
+
     } catch(error) {
       // Consider adjusting the error handling logic for your use case
       if (error.response) {
@@ -33,4 +58,4 @@ export const handleUrl =  async (req, res) => {
         });
       }
     }
-  }
+}
